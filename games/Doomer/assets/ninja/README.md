71a76632150c9da95a05d4bab47f5cdcc215c877
glitcj <gacc976431@gmail.com>
2026-03-02 15:52:07 +0900

    refactor to window, refactor to core, fleshout window, fleshout peekaboo, refactor prefixes to scene. getter. lambdas., exploit 100pct tilemap kotsu

---
+++ games/Doomer/assets/ninja/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6865198a6d7b052636269e9db0de966304737d5f8bacf683f2ac06ad60a68bd7
+size 599
--- games/Doomer/scenes/peekaboo/scene.peekaboo.gd
+++ games/Doomer/scenes/peekaboo/scene.peekaboo.gd
@@ -6,14 +6,26 @@ class_name _PeekaBoo
 @onready var lambdas = %Lambdas as _PeekaBoo_Lambdas
 @onready var getter = %Getter as _PeekaBoo_Getter
 
+@export var player_speed = 10
+@onready var player =  %Player as CharacterBody2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await lambdas._slide_windows_in()
-	"""
-	for _message_box : _Doomer_Message_Box in [logger]:
-		await _message_box.play_enumation(_Doomer_Message_Box.Enumations.Buzz)
-	"""
+
+
+func get_input():
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	player.velocity = direction * player_speed
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	# player.move_and_slide()
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	player.move_and_collide(direction * player_speed)
