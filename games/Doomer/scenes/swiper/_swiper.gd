0d0f693256747176bc63b46ae5901c88dfb299e4
glitcj <gacc976431@gmail.com>
2026-04-15 16:45:55 +0900

    finish swiper wheel peekaboo object + finish shaders _swiper + LR image container in _core_window + tales messages + fill maps and finish rpgmaker

--- games/Doomer/scenes/swiper/_swiper.gd
+++ games/Doomer/scenes/swiper/_swiper.gd
@@ -7,16 +7,25 @@ class_name _Swiper
 signal option_selected(selection)
 
 
-var correct_answer_index
-var incorrect_answer_index
+# var correct_answer_index
+# var incorrect_answer_index
+
+
+var verses := []
+var total_verses_on_page = 5
+# const var verses_on_page = 5
 
 var selection : String
 
-var questions_counter
+var scroll_counter = 100
 
 
 var koran_loader : _Core_Data_Lambdas
 var start_ayah_index
+
+var visible_labels : Array = []
+var visible_indices : Array = []
+
 
 func _on_scene_end():
 	super()
@@ -28,42 +37,67 @@ func _ready() -> void:
 	for label : Label in find_children("*", "Label"):
 		label.visible = false
 	
-		
-
-
-
-func _show_questions_and_answers(start_ayah_index):
-		
-	correct_answer_index = 0
-	incorrect_answer_index = int(not correct_answer_index)
-
-
-	print(correct_answer_index, incorrect_answer_index, "_word_%s" % correct_answer_index, "_word_%s" % incorrect_answer_index)
-	(find_child("_definition") as Label).text = koran_loader.quran_db[start_ayah_index]["ayah_ar"]
-	(find_child("_word_%s" % correct_answer_index) as Label).text = koran_loader.quran_db[start_ayah_index + 1]["ayah_ar"]
-	(find_child("_word_%s" % incorrect_answer_index) as Label).text = koran_loader.quran_db[start_ayah_index + 2]["ayah_ar"]
+	koran_loader = _Core_Data_Lambdas.new()
+	koran_loader.load_quran_csv("res://assets/kooran_de_go/quran.csv")
 	
 
-	await _Core_Tweener._slide_in(find_child("_definition"), .5)
-	await _Core_Tweener._slide_in(find_child("_word_0"), .5, Vector2i(-1, 0))
-	await _Core_Tweener._slide_in(find_child("_word_1"), .5, Vector2i(1, 0))
+
+
+func _initiate_visible_labels(start_ayah_index):
+	var verse
+	var scroll_position
+	
+	for i in range(total_verses_on_page):
+		visible_indices.append(start_ayah_index + i)
+		visible_labels.append(koran_loader.quran_db[start_ayah_index + i]["ayah_ar"])
+		
+		
+		
+		# verse = Label.new().label_settings = LabelSettings.new()
+		
+		verse = find_child("_verse_%s" % i) as Node2D
+		verses.append(verse)
+		scroll_position = find_child("_scroll_position_%s" % i) as CenterContainer
+		
+		# print(visible_labels)
+		verse.find_child("Label").text = visible_labels[i]
+		verse.find_child("Label").visible = true
+		# await _Core_Tweener._slide_in(find_child("_verse_%s" % i), .5)
+		
+		var tweener = verse.create_tween()
+		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+		await tweener.finished
+		print(visible_indices, visible_labels, verse.global_position)
 
 
 func _on_scene_start():
 	super()
 	await get_tree().process_frame
-
-
-
 	
-	questions_counter = 3
-	
-	koran_loader = _Core_Data_Lambdas.new()
-
-	koran_loader.load_quran_csv("res://assets/kooran_de_go/quran.csv")
-	
+	_reset_scroll_counter()
 	start_ayah_index = 1 + (randi() % koran_loader.quran_db.size())
-	_show_questions_and_answers(start_ayah_index)
+	_initiate_visible_labels(start_ayah_index)
+	
+	
+func _scroll_down():
+	var verse : Node2D
+	var scroll_position : CenterContainer
+	for i in range(total_verses_on_page):
+		verse = verses[i] as Node2D
+		scroll_position = find_child("_scroll_position_%s" % ((i + 1) % total_verses_on_page))
+		var tweener = verse.create_tween()
+		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+	verses.insert(0, verses.pop_back())
+	
+func _scroll_up():
+	var verse : Node2D
+	var scroll_position : CenterContainer
+	for i in range(total_verses_on_page):
+		verse = verses[i] as Node2D
+		scroll_position = find_child("_scroll_position_%s" % (  range(total_verses_on_page)[(i - 1) % total_verses_on_page]  ))
+		var tweener = verse.create_tween()
+		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+	verses.append(verses.pop_front())
 
 func _input(event: InputEvent) -> void:
 	if not is_active: return
@@ -72,34 +106,18 @@ func _input(event: InputEvent) -> void:
 	
 	match (event as InputEventKey).keycode:
 		KEY_RIGHT:
-			selection = "_word_0"
-			await _Core_Tweener._slide_out(find_child("_word_0"), .5, Vector2i(1, 0))
-			await _show_decision()
 			
-			await _Core_Tweener._slide_out(find_child("_word_1"), .5, Vector2i(0, 1))
-			await _Core_Tweener._slide_out(find_child("_definition"), .5, Vector2i(0, 1))
-			
-			
-			if questions_counter > 0:
-				start_ayah_index = start_ayah_index + 1
-				questions_counter = questions_counter - 1
-				await _show_questions_and_answers(start_ayah_index)
+			if scroll_counter > 0:
+				scroll_counter = scroll_counter - 1
+				await _scroll_down()
 			else:
 				option_selected.emit("_word_0")
 
 			
 		KEY_LEFT:
-			selection = "_word_1"
-			await _Core_Tweener._slide_out(find_child("_word_1"), .5, Vector2i(-1, 0))
-			await _show_decision()
-			
-			await _Core_Tweener._slide_out(find_child("_word_0"), .5, Vector2i(0, 1))
-			await _Core_Tweener._slide_out(find_child("_definition"), .5, Vector2i(0, 1))
-			
-			if questions_counter > 0:
-				start_ayah_index = start_ayah_index + 1
-				questions_counter = questions_counter - 1
-				await _show_questions_and_answers(start_ayah_index)
+			if scroll_counter > 0:
+				scroll_counter = scroll_counter - 1
+				await _scroll_up()
 			else:
 				option_selected.emit("_word_1")
 		_:
@@ -107,7 +125,7 @@ func _input(event: InputEvent) -> void:
 
 
 func _show_decision():
-	if selection == "_word_%s" % correct_answer_index:
+	if selection == "_verse_%s" % 0:
 		%_marubatsu.frame = 0
 	else:
 		%_marubatsu.frame = 1
@@ -115,3 +133,7 @@ func _show_decision():
 	await _Core_Tweener._slide_in(%_marubatsu)
 	await get_tree().create_timer(.25).timeout
 	await _Core_Tweener._slide_out(%_marubatsu)
+
+
+func _reset_scroll_counter():
+	scroll_counter = 100
