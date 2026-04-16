727d0235b61f0a887464bfd7cc8f7f7b8f121aad
glitcj <gacc976431@gmail.com>
2026-04-16 14:37:28 +0900

    finish swiper wheel peekaboo object + finish shaders _swiper + LR image container in _core_window + tales messages + fill maps and finish rpgmaker

--- games/Doomer/scenes/swiper/_swiper.gd
+++ games/Doomer/scenes/swiper/_swiper.gd
@@ -7,9 +7,7 @@ class_name _Swiper
 signal option_selected(selection)
 
 
-# var correct_answer_index
-# var incorrect_answer_index
-
+var interrupt_scroll = true
 
 var verses := []
 var total_verses_on_page = 5
@@ -33,10 +31,6 @@ func _on_scene_end():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera = %Camera2D as Camera2D
-	
-	# for label : Label in find_children("*", "Label"):
-	# 	label.visible = false
-	
 	koran_loader = _Core_Data_Lambdas.new()
 	koran_loader.load_quran_csv("res://assets/kooran_de_go/quran.csv")
 	
@@ -52,10 +46,13 @@ func _initiate_visible_labels(start_ayah_index):
 		verse.index = start_ayah_index + i
 		verse.text = koran_loader.quran_db[start_ayah_index + i]["ayah_ar"]
 		verses.append(verse)
-		scroll_position = find_child("_scroll_position_%s" % i) as CenterContainer
+		scroll_position = find_child("_scroll_position_%s" % i) as Node2D
 		
 		var tweener = verse.create_tween()
 		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+		tweener.parallel().tween_property(verse, "modulate", scroll_position.applied_modulate, .25)
+		tweener.parallel().tween_property(verse, "scale", scroll_position.applied_scale, .25)
+		
 		await tweener.finished
 		print(visible_indices, visible_labels, verse.global_position)
 		
@@ -63,11 +60,11 @@ func _on_scene_start():
 	super()
 	await get_tree().process_frame
 	
-	_reset_scroll_counter()
-	# start_ayah_index = 1 + (randi() % koran_loader.quran_db.size())
-	start_ayah_index = 1 # + (randi() % koran_loader.quran_db.size())
-	_initiate_visible_labels(start_ayah_index)
 	
+	_reset_scroll_counter()
+	start_ayah_index = 1 # + (randi() % koran_loader.quran_db.size())
+	await _initiate_visible_labels(start_ayah_index)
+	interrupt_scroll = false
 	
 func _scroll_down():
 	
@@ -75,19 +72,24 @@ func _scroll_down():
 		return
 		
 	var verse : Node2D
-	var scroll_position : CenterContainer
+	var scroll_position : Node2D
 	for i in range(total_verses_on_page):
 		verse = verses[i] as Node2D
 		scroll_position = find_child("_scroll_position_%s" % ((i + 1) % total_verses_on_page))
 		
 
+			
+		var tweener = verse.create_tween()
+		
+		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+		tweener.parallel().tween_property(verse, "modulate", scroll_position.applied_modulate, .25)
+		tweener.parallel().tween_property(verse, "scale", scroll_position.applied_scale, .25)
+		
 		if i == total_verses_on_page - 1:
 			verse.index = verses[0].index - 1
 			verse.text = koran_loader.quran_db[verse.index]["ayah_ar"]
 			
-			
-		var tweener = verse.create_tween()
-		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+		
 	verses.insert(0, verses.pop_back())
 	
 func _scroll_up():
@@ -95,17 +97,21 @@ func _scroll_up():
 		return
 		
 	var verse : Node2D
-	var scroll_position : CenterContainer
+	var scroll_position : Node2D
 	for i in range(total_verses_on_page):
 		verse = verses[i] as Node2D
-		scroll_position = find_child("_scroll_position_%s" % (  range(total_verses_on_page)[(i - 1) % total_verses_on_page]  ))
+		scroll_position = find_child("_scroll_position_%s" % (range(total_verses_on_page)[(i - 1) % total_verses_on_page]))
+		
+
+		var tweener = verse.create_tween()
+		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
+		tweener.parallel().tween_property(verse, "modulate", scroll_position.applied_modulate, .25)
+		tweener.parallel().tween_property(verse, "scale", scroll_position.applied_scale, .25)
 		
 		if i == 0:
 			verse.index = verses[-1].index + 1
 			verse.text = koran_loader.quran_db[verse.index]["ayah_ar"]
 			
-		var tweener = verse.create_tween()
-		tweener.tween_property(verse, "global_position", scroll_position.global_position, .25)
 		
 	verses.append(verses.pop_front())
 
@@ -113,6 +119,7 @@ func _input(event: InputEvent) -> void:
 	if not is_active: return
 	if not event is InputEventKey: return
 	if not (event.pressed and not event.echo): return
+	if interrupt_scroll: return
 	
 	match (event as InputEventKey).keycode:
 		KEY_RIGHT:
