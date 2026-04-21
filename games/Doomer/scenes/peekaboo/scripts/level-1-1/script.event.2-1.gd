9d8a99beb4961a99a00d0a6e4356654d54e11f4a
glitcj <gacc976431@gmail.com>
2026-04-21 20:09:59 +0900

    finish shaders _swiper + LR image container in _core_window + tales messages + fill maps and finish rpgmaker

--- games/Doomer/scenes/peekaboo/scripts/level-1-1/script.event.2-1.gd
+++ games/Doomer/scenes/peekaboo/scripts/level-1-1/script.event.2-1.gd
@@ -11,12 +11,6 @@ func _on_scene_start():
 func _on_frame():
 	if get_variables().l2_enemies_count == 2:
 		await get_peekaboo().get_lambdas().show_messages(["NPC: You got them !", "NPC: Go to the next location.."])
-		
-		# get_peekaboo().find_child()
-		# get_map().find_child("_player_position_4_1")
-		# get_map().find_child("_camera_position_4_1")
-		# get_lambdas().transport_player(get_variables().l3_entry_position)
-		# get_lambdas().transport_camera(get_variables().l3_entry_position)
-		get_lambdas().transport_player(get_map().find_child("_player_position_4_1"))
-		get_lambdas().transport_camera(get_map().find_child("_camera_position_4_1"))
+		get_lambdas().transport_player(get_map().find_child("_player_position_5_1"))
+		get_lambdas().transport_camera(get_map().find_child("_camera_position_5_1"))
 		parent.queue_free()
