ce349c6ee5f8c942bd183bcb7e52b4a6b6153817
glitcj <gacc976431@gmail.com>
2026-05-15 20:05:23 +0900

    get direction of velocity from joystick _input in _birds + finsh 1 statich left to right level where bird flies through things +  add slowing down of bird when ENTER + add apparition shader animation in Rockman + fleshout the hall of minis ; fill maps dont add maps + finish event movement lambdas in rpgm + refactor to core vieport to horizontal + refactor core portrait + finish rpgmaker map

--- games/Doomer/scenes/birds/_birds.gd
+++ games/Doomer/scenes/birds/_birds.gd
@@ -1,7 +1,14 @@
 extends _Core_Viewport
 class_name _Birds
 
+
+
+
+var birds_direction = Vector2(0,0)
+var lead_bird_direction = Vector2(0,0)
+
 signal finished
+var grid_size = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,12 +18,30 @@ func _ready() -> void:
 	for portrait : _RPGM_Portrait in find_children("*","_RPGM_Portrait"):
 		portrait.scale = Vector2(5, 5)
 		portrait.face_down()
+		
+	_loop_timer()
+
+func _loop_timer():
+	await get_tree().create_timer(1.).timeout
 	
+	if is_active:
+		var possible_directions = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)]
+		birds_direction = possible_directions[randi() % possible_directions.size()]
+		for bird : _RPGM_Portrait in find_children("_portrait","_RPGM_Portrait"):
+			move_bird(bird, bird.global_position + birds_direction * 50)
+			
+	_loop_timer()
 func _on_viewport_start():
 	super()
 	
 func _on_viewport_end():
 	super()
+	
+func move_bird(bird : _RPGM_Portrait, _position: Vector2):
+	var tween = get_tree().create_tween()
+	tween.tween_property(bird, "global_position", _position, .1)
+	await tween.finished
+	
 	
 func _input(event: InputEvent) -> void:
 	if not is_active: return
@@ -25,13 +50,26 @@ func _input(event: InputEvent) -> void:
 	
 	match (event as InputEventKey).keycode:
 		KEY_RIGHT:
-			pass
+			lead_bird_direction = Vector2(1, 0)
 			
 		KEY_LEFT:
-			pass
-
+			lead_bird_direction = Vector2(-1, 0)
+			
+		KEY_UP:
+			lead_bird_direction = Vector2(0, -1)
+			
+		KEY_DOWN:
+			lead_bird_direction = Vector2(0, 1)
+			
+		KEY_ENTER:
+			lead_bird_direction = Vector2(0, 0)
+			
 		KEY_ESCAPE:
 			finished.emit()
 			
 		_:
 			pass
+			
+func _physics_process(delta: float) -> void:
+	%_lead_bird.velocity = lead_bird_direction * 100
+	%_lead_bird.move_and_slide()
