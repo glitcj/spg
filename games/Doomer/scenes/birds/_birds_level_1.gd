ea66873ec62023d7f4418c252cf3f6788adba14b
glitcj <gacc976431@gmail.com>
2026-05-18 16:29:41 +0900

    get direction of velocity from joystick _input in _birds + finsh 1 statich left to right level where bird flies through things +  add slowing down of bird when ENTER + add apparition shader animation in Rockman + fleshout the hall of minis ; fill maps dont add maps + finish event movement lambdas in rpgm + refactor to core vieport to horizontal + refactor core portrait + finish rpgmaker map

--- games/Doomer/scenes/birds/_birds_level_1.gd
+++ games/Doomer/scenes/birds/_birds_level_1.gd
@@ -4,32 +4,25 @@ class_name _Birds_Level
 
 
 func _get_viewport(): return find_parent("_Birds") as _Birds
-
-
 var birds_direction = Vector2(0,0)
-var lead_bird_direction = Vector2(0,0)
+# var lead_bird_direction = Vector2(0,0)
+# var player_input_movement
+# var passive_movement = Vector2(300, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# camera = %Camera2D as Camera2D
-	%_portrait.animation_player.play("move_down")
-	
-
+	# %_portrait.animation_player.play("move_down")
+	pass
 	
 func _input(event: InputEvent) -> void:
 	if not _get_viewport().is_active: return
 	if not (event is InputEventKey): return
 	if not (event.pressed and not event.echo): return
 	
-	
-	
 	# Replace "ui_..." with your custom action names from the Input Map
-	var _direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
-	if _direction:
-		lead_bird_direction = _direction
-	else:
-		pass
+	# var _direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 		
 	match (event as InputEventKey).keycode:
 		KEY_RIGHT:
@@ -49,7 +42,8 @@ func _input(event: InputEvent) -> void:
 			pass
 			
 		KEY_ENTER:
-			lead_bird_direction = Vector2(0, 0)
+			# lead_bird_direction = Vector2(0, 0)
+			pass
 			
 		KEY_ESCAPE:
 			_get_viewport().finished.emit()
@@ -58,5 +52,7 @@ func _input(event: InputEvent) -> void:
 			pass
 			
 func _physics_process(delta: float) -> void:
-	%_lead_bird.velocity = lead_bird_direction * 100
-	%_lead_bird.move_and_slide()
+	if not _get_viewport().is_active: return
+	# player_input_movement = lead_bird_direction * 100
+	# %_lead_bird.velocity = player_input_movement + passive_movement
+	# %_lead_bird.move_and_slide()
---
+++ games/Doomer/scenes/birds/_birds_player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+class_name _Birds_Player
+
+func _get_viewport(): return find_parent("_Birds") as _Birds
+
+
+var birds_direction = Vector2(0,0)
+var player_input_direction = Vector2(0,0)
+var player_input_accumulated_direction = Vector2(0,0)
+var player_input_movement
+var passive_movement = Vector2(300, 0)
+
+func _ready() -> void:
+	%_RPGM_Portrait.face_down()
+	
+func _process(delta : float):
+	pass
+	
+func _process_echoed_input():
+	player_input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
+	player_input_accumulated_direction += player_input_direction * 1
+	
+func _input(event: InputEvent) -> void:
+	if not _get_viewport().is_active: return
+	if not (event is InputEventKey): return
+	_process_echoed_input()
+	
+	if not (event.pressed and not event.echo): return
+	
+func _physics_process(delta: float) -> void:
+	if not _get_viewport().is_active: return
+	player_input_movement = player_input_accumulated_direction * 100
+	
+	if delta - floor(delta)  < 0.1:
+		# _process_physics is quantized every 0.1, which makes direction change detection quirky
+		pass
+	
+	# if the direction flips..
+	if (velocity.x  * (player_input_movement + passive_movement).x) <= 0:
+		(%_RPGM_Portrait.find_child("Sprite2D") as Sprite2D).flip_h = player_input_direction.x < 0
+
+	velocity = player_input_movement + passive_movement
+	move_and_slide()
