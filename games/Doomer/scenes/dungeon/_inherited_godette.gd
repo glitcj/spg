73c112bb38106cfb83b255764b4397336b7755cb
glitcj <gacc976431@gmail.com>
2026-05-31 19:23:30 +0900

    Add two 3D rooms in 2D map, 2D to 3D teleport and vice versa + Add _Core_Log Vecto3 + REFACTOR RPGM + ADD 3D MINI GAME + add vector3 core log + add rockman + fles out birds enemies with masks and hit hurt box + finish birds level 1 + add apparition shader animation in Rockman + fleshout the hall of minis ; fill maps dont add maps + finish event movement lambdas in rpgm + refactor core portrait + finish rpgmaker map

---
+++ games/Doomer/scenes/dungeon/_inherited_godette.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# %AnimationPlayer.play("Idle")
+	
+	# %AnimationTree.set("parameters/conditions/dodge", true)
+	%AnimationTree.set("parameters/conditions/idle", true)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
