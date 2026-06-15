4718b90328b57741075d278ac96661a7d1b185a2
glitcj <gacc976431@gmail.com>
2026-06-15 16:24:53 +0900

    add room with persona block pushing + finish RPGM Refactor for cleanup and scale standardisation + Add _DGM_Mover + Add _DGM_Event + Add _Core_Log Vecto3 + REFACTOR RPGM + ADD 3D MINI GAME + add vector3 core log + add rockman + fles out birds enemies with masks and hit hurt box + finish birds level 1 + add apparition shader animation in Rockman + fleshout the hall of minis ; fill maps dont add maps + finish event movement lambdas in rpgm + refactor core portrait + finish rpgmaker map

--- games/Doomer/scenes/peekaboo/components/_rpgm_event.gd
+++ games/Doomer/scenes/peekaboo/components/_rpgm_event.gd
@@ -2,9 +2,7 @@
 extends Node2D
 class_name _RPGM_Event
 
-
-
-@export var is_collision = true
+var is_collision = true
 
 func get_rpgm(): return find_parent("_RPGM") as _RPGM
 func get_map(): return find_parent("_RPGM_Map") as _RPGM_Map
@@ -17,3 +15,35 @@ func get_portrait(): return find_child("_RPGM_Portrait") as _RPGM_Portrait
 
 func _ready():
 	pass
+
+func _child_entered_tree(_node: Node) -> void:
+	notify_property_list_changed()
+
+func _child_exiting_tree(_node: Node) -> void:
+	notify_property_list_changed()
+
+func _get_property_list() -> Array[Dictionary]:
+	if not is_inside_tree(): return []
+	var props: Array[Dictionary] = []
+	if get_mover():
+		props.append({
+			"name": "Mover",
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_CATEGORY
+		})
+		props.append({
+			"name": "is_collision",
+			"type": TYPE_BOOL,
+			"usage": PROPERTY_USAGE_DEFAULT
+		})
+	return props
+
+func _get(property: StringName) -> Variant:
+	if property == "is_collision": return is_collision
+	return null
+
+func _set(property: StringName, value: Variant) -> bool:
+	if property == "is_collision":
+		is_collision = value
+		return true
+	return false
